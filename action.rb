60cfab6d12b53123e6adffc361a1b8ed4bdd62e6
J. Fernando Sánchez <jf.sanchez@upm.es>
2025-02-04 11:25:09 +0100

    Add option to allow inviting already existing members

--- action.rb
+++ action.rb
@@ -8,6 +8,7 @@ org = ENV['ORG']
 team_id = ENV['TEAM_ID']
 commenter = event["comment"]["user"]["login"]
 self_invite = defined?(ENV['SELF_INVITE']) == nil ? true : ENV['SELF_INVITE']
+invite_existing = defined?(ENV['INVITE_EXISTING']) == nil ? true : ENV['INVITE_EXISTING']
 
 puts "-------------------------------------------------"
 
@@ -16,13 +17,20 @@ client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
 
 if comment.include?(".invite") && comment.split().length == 2
   cmd, handle = comment.split
-  
-  user = ( handle == "me" && self_invite ? commenter : handle.tr('@', '') )
-  exit(78) if ( user == commenter ) && self_invite
-    
-  puts "USER: #{user}"
 
-  exit(78) if client.organization_member?(org, user)
+
+  user = ( handle == "me" && self_invite ? commenter : handle.tr('@', '') )
+  puts "USER: #{user}"
+  
+  if ( user == commenter ) && self_invite 
+    puts "Self-inviting is not enabled"
+    exit(78) 
+  end
+  
+  if client.organization_member?(org, user) && invite_existing
+    puts "User is already a member of the organization, and inviting existing members is not enabled."
+    exit(99) 
+  end
   
   client.add_team_membership(team_id, user)
 end
